acca2ca17da738f34c77cc69c9274863abb6dbbf
Jan (kisterl) <jan@veskamyr.de>
2015-03-19 15:54:21 +0100

    Brought documentation to a publishable status.
    Documentation was extended to contain information about development
    status, mailing lists, and github repository. Moreover, graphics were
    created.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -108,7 +108,11 @@ html_theme = 'scrolls'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {}
+html_theme_options = {
+    "headerbordercolor": "#4a236a",
+    "subheadlinecolor": "#4a236a",
+    #"admonitioncolor": ,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
